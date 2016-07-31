c0f102009a5ae93a6a596fedc2b4ce60c5b5f5f7
Bernhard Landauer <oberon@manjaro.org>
2016-07-31 23:57:09 +0200

    adjust [linux-rt-lts-manjaro]

--- linux-rt-lts-manjaro-extramodules/build-extramodules.sh
+++ linux-rt-lts-manjaro-extramodules/build-extramodules.sh
@@ -20,7 +20,7 @@ cd ${pwd}/*nvidia-304xx && makepkg -d --noconfirm
 #cd ${pwd}/*nvidia-340xx && makepkg -d --noconfirm
 cd ${pwd}/*nvidiabl && makepkg -sf --noconfirm
 cd ${pwd}/*open-vm-tools-modules && makepkg -sf --noconfirm
-cd ${pwd}/*spl_zfs && makepkg -sf --noconfirm
+#cd ${pwd}/*spl_zfs && makepkg -sf --noconfirm
 cd ${pwd}/*r8168 && makepkg -sf --noconfirm
 cd ${pwd}/*rt3562sta && makepkg -sf --noconfirm
 cd ${pwd}/*tp_smapi && makepkg -sf --noconfirm
